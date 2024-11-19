a9fb2642fa87ce28a93939d8d1685d103d8712f7
GrigoryanDav <grigoryand219@gmail.com>
2024-11-19 19:02:51 +0400

    Put Calculator component into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-function App() {
+import { FC } from "react";
+import Calculator from "./components/Calculator";
+import './App.css'
+
+const App: FC = () => {
   return (
     <div className="App">
-
+      <h1>Calculator</h1>
+      <Calculator />
     </div>
   );
 }
