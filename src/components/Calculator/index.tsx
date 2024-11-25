3edb5ff2689cb3b2c0251e32805a25c5ca665dc9
GrigoryanDav <grigoryand219@gmail.com>
2024-11-25 17:29:50 +0400

    Add constants for calculator buttons values

--- src/components/Calculator/index.tsx
+++ src/components/Calculator/index.tsx
@@ -2,6 +2,7 @@ import { FC, useState } from "react"
 import { evaluate } from "mathjs"
 import Display from "../Display"
 import Button from "../Button"
+import { buttonGroups } from "../../constants/buttonGroups"
 import './index.css'
 
 const Calculator: FC = () => {
@@ -30,30 +31,16 @@ const Calculator: FC = () => {
             <Display value={result || input || '0'} />
             <div className="buttons">
                 {
-                    ['7', '8', '9', '/'].map((btn) => (
-                        <Button key={btn} label={btn} onClick={() => handleButtonClick(btn)} />
+                    buttonGroups.map((group, groupIndex) => (
+                        <div key={groupIndex}>
+                            {
+                                group.map((btn) => (
+                                    <Button key={btn} label={btn} onClick={() => handleButtonClick(btn)} />
+                                ))
+                            }
+                        </div>
                     ))
                 }
-
-                {
-                    ['4', '5', '6', '*'].map((btn) => (
-                        <Button key={btn} label={btn} onClick={() => handleButtonClick(btn)} />
-                    ))
-                }
-
-                {
-                    ['1', '2', '3', '-'].map((btn) => (
-                        <Button key={btn} label={btn} onClick={() => handleButtonClick(btn)} />
-                    ))
-                }
-
-                {
-                    ['0', '.', '=', '+'].map((btn) => (
-                        <Button key={btn} label={btn} onClick={() => handleButtonClick(btn)} />
-                    ))
-                }
-
-                <Button label="C" onClick={() => handleButtonClick('C')} />
             </div>
         </div>
     )
